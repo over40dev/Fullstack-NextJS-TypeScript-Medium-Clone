3f53e505bf0e31bcb7f19317e6ad6258a72741f9
Cogentx <code@cogentx.com>
2022-01-27 14:24:16 -0330

    Implementing Next.js SSR and Post typings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,15 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import { sanityClient } from '../sanity';
+import { Post } from '../typings';
 
-export default function Home() {
+interface Props {
+  posts: [Post];
+}
+
+export default function Home({posts}: Props) {
+  console.log({posts});
+
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -31,3 +39,28 @@ export default function Home() {
     </div>
   );
 }
+
+// Enable SSR in Next.js for this page
+export const getServerSideProps = async () => {
+  // fetch data from Sanity
+  const query = `
+  *[_type == "post"] {
+_id,
+  title,
+  author -> {
+  name,
+  image
+  },
+description,
+mainImage,
+slug,
+}`;
+
+  const posts = await sanityClient.fetch(query);
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
