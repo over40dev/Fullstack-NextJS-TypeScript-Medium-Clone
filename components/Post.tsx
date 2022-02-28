9539874c086ead80e69be07bac7ef544b7509e3c
Cogentx <code@cogentx.com>
2022-02-28 17:19:46 -0330

    Complete Post | Posts with content | styling

--- components/Post.tsx
+++ components/Post.tsx
@@ -9,9 +9,26 @@ interface IProps {
 function Post({ post }: IProps) {
   return (
     <Link key={post._id} href={`/posts/${post.slug.current}`}>
-      <div>
+      <div className="group border rounded-lg cursor-pointer overflow-hidden">
         {/* if mainImage.asset.url exists render image | '!' at end of url() assures TypeScript value will exist */}
-        {post.mainImage && <img src={urlFor(post.mainImage).url()!} alt="post image" />}
+        {post.mainImage && (
+          <img
+            className="h-60 w-full object-cover transition-transform duration-200 ease-in-out group-hover:scale-105"
+            src={urlFor(post.mainImage).url()!}
+            alt="post image"
+          />
+        )}
+        <div className="flex justify-between bg-white p-5">
+          <div>
+            <p className="text-lg font-bold">{post.title}</p>
+            <p className="text-xs">
+              {post.description} by {post.author.name}
+            </p>
+          </div>
+          {post.author.image && (
+            <img className="h-12 w-12 rounded-full" src={urlFor(post.author.image).url()!} alt="author image" />
+          )}
+        </div>
       </div>
     </Link>
   );
