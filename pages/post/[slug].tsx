7ccd2ed56854f97f54c6c3312e141f3dd7cd52b1
Cogentx <code@cogentx.com>
2022-03-01 20:28:53 -0330

    Form Functionality with React-Hook-Form

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -1,15 +1,32 @@
 import { GetStaticProps } from 'next/types';
 import React from 'react';
+import PortableText from 'react-portable-text';
+import { useForm, SubmitHandler } from 'react-hook-form';
 import Header from '../../components/Header';
 import { sanityClient, urlFor } from '../../sanity';
 import { IPost } from '../../typings';
-import PortableText from 'react-portable-text';
 
+interface IFormInput {
+  _id: string;
+  name: string;
+  email: string;
+  comment: string;
+}
 interface Props {
   post: IPost;
 }
 
 function PostPage({ post }: Props) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>();
+
+  const onSubmit: SubmitHandler<IFormInput> = async (data) => {
+    console.log({ data });
+  };
+
   return (
     <main>
       <Header />
@@ -54,16 +71,20 @@ function PostPage({ post }: Props) {
 
       <hr className="my-5 mx-auto max-w-lg border border-yellow-500" />
 
-      <form className="mx-auto mb-10 flex max-w-2xl flex-col p-5">
+      <form onSubmit={handleSubmit(onSubmit)} className="mx-auto mb-10 flex max-w-2xl flex-col p-5">
         <h3 className="text-sm text-yellow-500">Enjoyed the article?</h3>
         <h4 className="text-3xl font-bold">Leave a comment below!</h4>
         <hr className="mt-2 py-3" />
+
+        <input type="hidden" {...register('_id')} name="_id" value={post._id} />
+
         <label className="mb-5 block">
           <span className="text-gray-700">Name</span>
           <input
             className="focus:rin block w-full rounded border py-2 px-3  shadow outline-none ring-yellow-500 focus:ring"
             placeholder="J Doe"
             type="text"
+            {...register('name', { required: true })}
           />
         </label>
         <label className="mb-5 block">
@@ -72,6 +93,7 @@ function PostPage({ post }: Props) {
             className="focus:rin block w-full rounded border py-2 px-3  shadow outline-none ring-yellow-500 focus:ring"
             placeholder="j@doe.com"
             type="email"
+            {...register('email', { required: true })}
           />
         </label>
         <label className="mb-5 block">
@@ -80,8 +102,21 @@ function PostPage({ post }: Props) {
             className="form-textarea mt-1 block w-full rounded border py-2 px-3 shadow outline-none ring-yellow-500 focus:ring"
             placeholder="Your comments please..."
             rows={8}
+            {...register('comment', { required: true })}
           />
         </label>
+
+        {/* errors will return when field validation fails */}
+        <div>
+          {errors.name && <span className="text-red-500">- Name field is required</span>}
+          {errors.email && <span className="text-red-500">- Email field is required</span>}
+          {errors.comment && <span className="text-red-500">- Comment field is required</span>}
+        </div>
+
+        <input
+          type="submit"
+          className="shadow bg-yellow-500 hover:bg-yellow-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded cursor-pointer"
+        />
       </form>
     </main>
   );
